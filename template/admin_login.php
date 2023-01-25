fe326b140abb5ab47cb056afc415e8c2795e06b9
Jason Tampus <tampuscss10@gmail.com>
2023-01-25 14:43:32 +0800

    Update admin_login.php
    yes

--- template/admin_login.php
+++ template/admin_login.php
@@ -6,12 +6,13 @@
   
         $username = stripcslashes($username);  
         $password = stripcslashes($password);  
-        $username = mysqli_real_escape_string($con, $username);  
-        $password = mysqli_real_escape_string($con, $password);  
+        $username = mysqli_real_escape_string($conn, $username);  
+        $password = mysqli_real_escape_string($conn, $password);  
       
         $sql = " SELECT * FROM users WHERE username = '$username' AND password = '$password'";  
-        $result = mysqli_query($con, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $result = mysqli_query($conn, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $active = $row['active'];
         $count = mysqli_num_rows($result);  
           
         if($count == 1){  
