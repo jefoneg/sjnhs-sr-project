ee451536360573959d2496368339b1fd4ef177b1
Jefone Gorme <jefonegorme@gmail.com>
2023-01-25 15:40:47 +0800

    update adminlogin

--- template/admin_login.php
+++ template/admin_login.php
@@ -9,21 +9,40 @@
         $username = mysqli_real_escape_string($conn, $username);  
         $password = mysqli_real_escape_string($conn, $password);  
       
-        $sql = " SELECT * FROM users WHERE username = '$username' AND password = '$password'";  
-        $result = mysqli_query($conn, $sql);  
-        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-        $active = $row['active'];
-        $count = mysqli_num_rows($result);  
+        $sqladmin = " SELECT * FROM user WHERE username = '$username' AND password = '$password' AND position = 'admin'";
+        $sqlteacher = " SELECT * FROM user WHERE username = '$username' AND password = '$password' AND position = 'teacher'";   
+        $sqlstaff = " SELECT * FROM user WHERE username = '$username' AND password = '$password' AND position = 'staff'";   
+        $resultadmin = mysqli_query($conn, $sqladmin);
+        $resultteacher = mysqli_query($conn, $sqlteacher);  
+        $resultstaff = mysqli_query($conn, $sqlstaff);    
+        $rowadmin = mysqli_fetch_array($resultadmin, MYSQLI_ASSOC);
+        $rowteacher = mysqli_fetch_array($resultteacher, MYSQLI_ASSOC);
+        $rowstaff = mysqli_fetch_array($resultstaff, MYSQLI_ASSOC);
+        $countadmin = mysqli_num_rows($resultadmin);
+        $countteacher = mysqli_num_rows($resultteacher);  
+        $countstaff = mysqli_num_rows($resultstaff);    
           
-        if($count == 1){  
+        if($countadmin == 1){  
             $_SESSION['username'] = $username; 
             $_SESSION['password']= $password;
             $_SESSION['success']= 'You have successfully logged in!';
-            header('location: index.html');
-        }  
+            header('location:template/dashboard.php');
+        }
+        else if($countteacher == 1){  
+            $_SESSION['username'] = $username; 
+            $_SESSION['password']= $password;
+            $_SESSION['success']= 'You have successfully logged in!';
+            header('location:template/pages.html');
+        } 
+        else if($countstaff == 1){  
+            $_SESSION['username'] = $username; 
+            $_SESSION['password']= $password;
+            $_SESSION['success']= 'You have successfully logged in!';
+            header('location:index.html');
+        }
         else{  
             $_SESSION['warning'] = "Invalid email and password!";
-            header('location: admin_login.php'); 
+            header('location: index.php'); 
         }
 
 ?>
