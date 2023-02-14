6336553cbf5bf3c819fd2abe3bd254c73a290070
Jefone Gorme <jefonegorme@gmail.com>
2023-02-14 10:00:51 +0800

    Update sjnhs_db.sql

--- assets/db/sjnhs_db.sql
+++ assets/db/sjnhs_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jan 27, 2023 at 01:34 PM
+-- Generation Time: Feb 14, 2023 at 01:59 AM
 -- Server version: 5.7.33
 -- PHP Version: 8.0.24
 
@@ -29,7 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `student_tbl` (
   `student_id` int(50) NOT NULL,
-  `image` varchar(150) NOT NULL,
+  `image` varchar(255) NOT NULL,
   `firstname` varchar(20) NOT NULL,
   `lastname` varchar(20) NOT NULL,
   `section` varchar(20) NOT NULL,
@@ -42,12 +42,12 @@ CREATE TABLE `student_tbl` (
 --
 
 INSERT INTO `student_tbl` (`student_id`, `image`, `firstname`, `lastname`, `section`, `teacher_fname`, `teacher_lname`) VALUES
-(1, '', 'jefone', 'gorme', '11-A', 'ligaya', 'merquita'),
-(2, '', 'mark jason', 'tampus', '11-B', 'ralyn', 'deplomo'),
-(3, '', 'rheymark', 'quinones', '12-A', 'ligaya', 'merquita'),
-(4, '', 'ericah', 'quiban', '12-B', 'ralyn', 'deplomo'),
-(5, '', 'charlie', 'berido', '12-B', 'ligaya', 'merquita'),
-(6, '', 'alsen', 'abrenilla', '12-C', 'ralyn', 'deplomo');
+(2, 'DSC_4537.JPG', 'jefone', 'gorme', '11-ICT', 'ligaya', 'merquita'),
+(3, '253849909_4850958961686831_4993306614471978728_n.jpg', 'mark jason', 'tampus', '', 'ligaya', 'merquita'),
+(4, '317827590_445959797704499_6653160768263919326_n.jpg', 'alsen', 'abrenilla', '', 'ligaya', 'merquita'),
+(5, '253336612_4850962941686433_5255364686788358813_n.jpg', 'rheymark', 'quinones', '11-STEM-C', 'ligaya', 'merquita'),
+(6, '81692259_1363498353858346_286246874355073024_n.jpg', 'ericah', 'quiban', 'Null', 'ligaya', 'merquita'),
+(7, 'home-economics.jpg', 'test', 'test', 'Null', 'test', 'test');
 
 -- --------------------------------------------------------
 
@@ -62,17 +62,18 @@ CREATE TABLE `user` (
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
   `position` varchar(50) NOT NULL,
-  `image` varchar(150) NOT NULL
+  `image` varchar(255) NOT NULL,
+  `section` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `user`
 --
 
-INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `password`, `position`, `image`) VALUES
-(1, 'Jefone', 'Gorme', 'jefone', 'gorme', 'administrator', ''),
-(4, 'ligaya', 'merquita', 'ligaya', 'merquita', 'teacher', ''),
-(5, 'ralyn', 'deplomo', 'ralyn', 'deplomo', 'teacher', '');
+INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `password`, `position`, `image`, `section`) VALUES
+(12, 'jefone', 'gorme', 'jefone', 'gorme', 'Administrator', 'DSC_4537.JPG', ''),
+(17, 'ligaya', 'merquita', 'ligaya', 'merquita', 'Teacher', '254071436_4850967461685981_1889757169354950312_n.jpg', '11-STEM-C'),
+(18, 'ralyn', 'deplomo', 'root', 'root', 'Teacher', '317827590_445959797704499_6653160768263919326_n.jpg', '11-ICT-B');
 
 --
 -- Indexes for dumped tables
@@ -98,13 +99,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `student_tbl`
 --
 ALTER TABLE `student_tbl`
-  MODIFY `student_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `student_id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `user_id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
