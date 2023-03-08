6ddb712df757742d085840236dec0fcb719a0501
Charlie Magne N. Berido <114430348+Mirikashitakikumirin@users.noreply.github.com>
2023-03-08 10:58:13 +0800

    Update addstudent.php

--- addstudent.php
+++ addstudent.php
@@ -14,7 +14,7 @@ $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
 
 
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
-    $query = "INSERT INTO student_tbl(image,firstname,lastname,section,teacher_fname,teacher_lname,century_literature,oral_communication,wika_kulturang_pilipino,earth_life_science,genmath,personal_development,pe,empowerment,css1,contemporary,pe2,reading_writing,statistics,pananaliksik,entrep,practical_research1,css2,philosophy,ucsp,pe_health,filipino,practical_research2,english_app,css3,physical_science,mil,pe_health2,immersion,css4,work_immersion) VALUES ('$userfile','$firstname','$lastname','$section','$teacher_fname','$teacher_lname','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','')";
+    $query = "INSERT INTO student_tbl(image,firstname,lastname,section,teacher_fname,teacher_lname) VALUES ('$userfile','$firstname','$lastname','$section','$teacher_fname','$teacher_lname')";
     mysqli_query($conn,$query);
     $_SESSION['prompt'] = "Alert!";
     $_SESSION['success'] ="Sucessfully added!";
